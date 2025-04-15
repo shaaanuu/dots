f98d1b60a182d1626dce0283913a73222ce0fd90
shaaanuu <monshan50@gmail.com>
2025-04-15 19:37:36 +0530

    nvim: add shortcut of Typr in dashboard

--- nvim/.config/nvim/lua/plugins/dashboard.lua
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -29,10 +29,10 @@ return {
         },
 
         center = {
-          { icon = "󰈸  ", desc = "Hack NASA",        action = "!echo 'Access denied 🛑'",       key = "h" },
+          { icon = "󰈸  ", desc = "Typr",        action = "ene | Typr",       key = "t" },
           { icon = "  ", desc = "Write Code",      action = "ene | startinsert",             key = "w" },
           { icon = "󰊢  ", desc = "Procrastinate",   action = "!cowsay '5 more minutes...'",   key = "p" },
-          { icon = "󱎫  ", desc = "Touch Grass",     action = "!echo '404: Grass not found'",  key = "t" },
+          { icon = "󱎫  ", desc = "Touch Grass",     action = "!echo '404: Grass not found'",  key = "g" },
           { icon = "  ", desc = "rm -rf /",         action = "!echo '💀 Nice try, rootless'", key = "r" },
           { icon = "󰩈  ", desc = "Exit Neovim",     action = "qa",                            key = "q" },
         },
